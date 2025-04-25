a390f01a0d72ecc1621664e23742f160888c66b8
MikaelReis <mikaelreis08@gmail.com>
2025-04-25 20:00:00 -0300

    Atualizando Settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-cxv=48ygm8qi)yf$m!82ea(+@02gx5brb4*te-!g#i%j!^0zw%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","mikaelreis.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","MikaelReis.pythonanywhere.com"]
 
 
 # Application definition
